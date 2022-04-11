dc31830bfbebb3ad376a1dd568391210728ae467
SSstupid <rlawoghks3@gmail.com>
2022-04-11 14:32:07 +0900

    Up

---
+++ 질문 저장소/Fasetto.word.Framework/Fasetto.word.Framework/DataModels/ApplicationPage.cs
@@ -0,0 +1,18 @@
+﻿namespace Fasetto.word.Framework
+{
+    /// <summary>
+    /// A page of the application
+    /// </summary>
+    public enum ApplicationPage
+    {
+        /// <summary>
+        /// The initial login page
+        /// </summary>
+        Login,
+
+        /// <summary>
+        /// The main chat page
+        /// </summary>
+        Chat, 
+    }
+}
---
+++ 질문 저장소/Fasetto.word.Framework/Fasetto.word.Framework/Page/ChatPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security;
+using System.Windows;
+
+namespace Fasetto.word.Framework
+{
+    /// <summary>
+    /// ChatPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ChatPage : BasePage<LoginViewModel>
+    {
+        public ChatPage()
+        {
+            InitializeComponent();
+        }
+    } 
+}
